5e85f23181c7126721307cfad914fcc86a2aa864
Xtremind <guillaume.dagraca@gmail.com>
2025-10-07 22:42:07 +0200

    review eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 
+import globals from 'globals'
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+    {ignores: ['dist/**', 'node_modules/**']},
+    {languageOptions: {globals: 
+        {
+            ...globals.browser,
+            ...globals.node
+    
+        }
+    }},
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
 );
