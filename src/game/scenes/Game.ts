4121bc179645158907b6759b6c42a2a8156afd0a
Xtremind <guillaume.dagraca@gmail.com>
2025-10-21 22:06:34 +0200

    adding collision without altering speed

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -22,40 +22,29 @@ export class Game extends Scene
 
     create ()
     {
-                //this.camera = this.cameras.main;
-        //this.camera.setBackgroundColor(0xffffff);
-
-        //this.background = this.add.image(512, 384, 'background');
-        //this.background.setAlpha(0.5);
-
-
-        /*this.input.once('pointerdown', () => {
-            this.scene.start('GameOver');
-        });*/
-        //set world bounds
-        this.physics.world.setBounds(0, 0, this.cameras.main.width / this.cameras.main.zoom, this.cameras.main.height / this.cameras.main.zoom);
-        this.physics.world.setBoundsCollision();
+        //DEBUG MODE
         this.physics.world.defaults.bodyDebugColor = 0xff00ff;
         this.physics.world.defaults.velocityDebugColor = 0x00ff00;
         this.physics.world.defaults.staticBodyDebugColor = 0x0000ff;
 
+        //set world bounds
+        this.physics.world.setBounds(0, 0, this.cameras.main.width / this.cameras.main.zoom, this.cameras.main.height / this.cameras.main.zoom);
+        this.physics.world.setBoundsCollision();
 
         //initiate players
-
         this.#players = (this.sys.game as any).players;
-
         this.#players.forEach( (player: Player, index: integer) => {
-
+            //compute player position
             var playTable = { x0: this.cameras.main.worldView.x + this.cameras.main.width / (2 * this.cameras.main.zoom), y0: this.cameras.main.worldView.y + this.cameras.main.height / (2 * this.cameras.main.zoom), r: 50};
             var radius = (Math.PI * 2) / this.#players.length;
             var position = { x: playTable.x0 + playTable.r * Math.sin(radius * (index % this.#players.length)), y: playTable.y0 + playTable.r * Math.cos(radius * (index % this.#players.length)) };
-
+            //add player
             player.sprite = Graphics.addPlayer(this, player, position);
 
         });
 
         //set collision
-        console.log(this.#players);
+        this.physics.add.collider(this.#players.map(p => p.sprite), this.#players.map(p => p.sprite));
 
         //initialise default function
         this.#update = this.#update_game;
@@ -70,7 +59,10 @@ export class Game extends Scene
     }
 
     #update_game(){
-
+        //Tweak so that a moving player keeps the same speed
+        this.#players.forEach(player => {
+            (player.sprite.body as Phaser.Physics.Arcade.Body).velocity.normalize().scale(200);
+        });
     }
 
     #draw_game(){
